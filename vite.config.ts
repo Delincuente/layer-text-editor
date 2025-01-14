9bb8bb37cbdf4275b969f13f8827a48e02f00e0f
CRIMINAL <109715754+Delincuente@users.noreply.github.com>
2025-01-14 22:01:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: mode === '/',
+   base: '/',
   server: {
     host: "::",
-    port: 8080,
+    port: 5000,
   },
   plugins: [
     react(),
